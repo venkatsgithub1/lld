5edcc0a23c7a23fe32d8055d03665200d1e8330b
ven <venkatsgithub@gmail.com>
2026-02-17 23:16:10 +0530

    Added todo

--- consistenthashing/src/main/java/io/ven/hasher/ConsistentHasher.java
+++ consistenthashing/src/main/java/io/ven/hasher/ConsistentHasher.java
@@ -4,6 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
+// TODO: Add testing logic.
 public class ConsistentHasher<T> {
     private final TreeMap<Long, T> ring = new TreeMap<>();
     private final int numReplicas;
